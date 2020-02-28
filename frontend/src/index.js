96eaf806e747dbf983e842485ab6811e60c024c0
Dennis Padiernos <depadiernos@gmail.com>
2020-02-28 17:46:43 -0600

    completed Auth0 Frontend example.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import { styled } from "linaria/react"
-
+import { createBrowserHistory } from "history"
 import PrivateRoute from "./components/privateroute"
 import NavBar from "./components/navbar"
 import Dashboard from "./pages/dashboard"
 import { Auth0Provider, useAuth0 } from "./components/authwrapper"
 import config from "../../auth_config.json"
-import history from "./utils/history"
 
 // A function that routes the user to the right place
 // after login
+const history = createBrowserHistory()
 const onRedirectCallback = (appState) => {
   history.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname)
 }
@@ -26,7 +26,6 @@ const App = () => {
   return (
     <Style>
       <NavBar />
-      <h1>Hello World!</h1>
       <PrivateRoute exact path="/" component={Dashboard} />
     </Style>
   )
@@ -42,7 +41,7 @@ ReactDOM.render(
     client_id={config.clientId}
     redirect_uri={window.location.origin}
     onRedirectCallback={onRedirectCallback}>
-    <Router>
+    <Router history={history}>
       <App />
     </Router>
   </Auth0Provider>,
